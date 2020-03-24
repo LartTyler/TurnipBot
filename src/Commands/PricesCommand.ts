6935802f953059903fc73f6d4f58eb81baf8c0f5
Tyler Lartonoix <lord.codemonkey@gmail.com>
2020-03-23 20:48:20 -0400

    Refactor prices command to use source prices from UserInfo.currentData

--- src/Commands/PricesCommand.ts
+++ src/Commands/PricesCommand.ts
@@ -4,7 +4,7 @@ import {BuyData, IBuyData} from '../Models/BuyData';
 import {ISellData, SellData} from '../Models/SellData';
 import {UserInfo} from '../Models/UserInfo';
 import {ucfirst, WeekDay} from '../util';
-import {Command, TIMEZONE_REQUIRED_MESSAGE} from './index';
+import {Command} from './index';
 
 enum PriceType {
 	BUY = 'buy',
@@ -21,38 +21,63 @@ export const execute: Command = async (sender, args) => {
 		return;
 	}
 
+	const now = DateTime.utc();
 	const limit = args.length >= 1 ? parseInt(args[0], 10) || 1 : 1;
 
 	let type: string;
 
-	if (args.length >= 2) {
+	if (args.length >= 2)
 		type = args[1];
+	else if (now.weekday === WeekDay.SUNDAY)
+		type = PriceType.SELL;
+	else
+		type = PriceType.BUY;
 
-		if (!(type in PriceType)) {
-			await sender.message.reply('Sorry, I didn\'t quite get that. Allowed price types are "buy" and "sell."');
+	let lines: string[];
 
-			return;
-		}
-	} else {
-		if (!sender.userInfo?.timezone) {
-			await sender.message.reply(TIMEZONE_REQUIRED_MESSAGE);
-
-			return;
-		}
-
-		const now = DateTime.fromObject({zone: sender.userInfo.timezone});
-
-		if (now.weekday === WeekDay.SUNDAY)
-			type = PriceType.SELL;
-		else
-			type = PriceType.BUY;
-	}
-
-	const lines = (await (type === PriceType.BUY ? getBestBuyPrices : getBestSellPrices)(guild.id, limit))
-		.map(item => formatPriceLine(
+	if (type === PriceType.BUY) {
+		lines = (await UserInfo.find(
+			{
+				serverIds: guild.id,
+				'currentData.buyExpiration': {
+					'$gt': now.toJSDate(),
+				},
+			},
+			'-serverIds',
+			{
+				limit,
+				sort: {
+					'currentData.buyPrice': -1,
+				},
+			},
+		)).map(item => formatPriceLine(
 			guild.member(item.userId)?.displayName || '[redacted]',
-			item.price,
+			item.currentData.buyPrice,
 		));
+	} else if (type === PriceType.SELL) {
+		lines = (await UserInfo.find(
+			{
+				serverIds: guild.id,
+				'currentData.sellExpiration': {
+					'$gt': now.toJSDate(),
+				},
+			},
+			'-serverIds',
+			{
+				limit,
+				sort: {
+					'currentData.sellPrice': 1,
+				},
+			},
+		)).map(item => formatPriceLine(
+			guild.member(item.userId)?.displayName || '[redacted]',
+			item.currentData.sellPrice,
+		));
+	} else {
+		await sender.message.reply('Sorry, I didn\'t quite get that. Allowed price types are "buy" and "sell."');
+
+		return;
+	}
 
 	if (lines.length === 0)
 		await sender.channel.send(`No one has reported any ${type} prices yet.`);
@@ -69,6 +94,12 @@ function formatPriceLine(name: string, price: number) {
 	return `${price} bells on ${name}'s island`;
 }
 
+async function getUsersForServer(serverId: string): Promise<string[]> {
+	return (await UserInfo.find({
+		serverIds: serverId,
+	}, '+userId -_id')).map(item => item.userId);
+}
+
 async function getBestSellPrices(serverId: string, limit: number = 1): Promise<ISellData[]> {
 	return getBestPrices(SellData, serverId, 1, limit);
 }
@@ -83,12 +114,16 @@ async function getBestPrices<T extends IBuyData | ISellData>(
 	sort: -1 | 1,
 	limit: number = 1,
 ): Promise<T[]> {
+	const users = (await UserInfo.find({
+		serverIds: serverId,
+	}, '+userId -_id')).map(item => item.userId);
+
+	console.debug(users);
+
 	return (await source.find(
 		{
 			userId: {
-				'$in': (await UserInfo.find({
-					serverIds: serverId,
-				}, '_id')).map(item => item.id),
+				'$in': users,
 			},
 		} as FilterQuery<T>,
 		null,
